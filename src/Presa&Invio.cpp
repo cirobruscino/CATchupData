d608e5d21cf0fa475d6472abd3f3d71b041d238b
Ciro Bruscino <ciro.bruscino@hotmail.com>
2022-01-22 00:55:27 +0100

    DataCATchup ver. 1.0.1

--- src/Presa&Invio.cpp
+++ src/Presa&Invio.cpp
@@ -1,3 +1,6 @@
+//DataCATchup ver. 1.0.0   Created Run&Send code for CATchup project
+//DataCATchup ver. 1.0.1   Delay controller condition and timestamp update added
+
 #include <Arduino_LSM6DS3.h>
 #include <ArduinoHttpClient.h>
 #include <WiFiNINA.h>
@@ -25,6 +28,9 @@ float g_x;
 float g_y; 
 float g_z;
 
+// Fisso la frequenza di presa dati tramite il periodo, espresso in microsecondi
+int Periodo = 40000;
+
 // Inizializzo la stringa di dati composta dalle 3 componenti dell'accelerazione e dalle 3 componenti della velocità angolare
 String StringaDati = "";
 
@@ -35,8 +41,11 @@ String DataMatrix ="\"";
 int i = 0;
 
 // Inizializzo le variabili per il timestamp
-float Time;
 String response;
+int IndiceSeparatore;
+int TimestampMicros;
+int TimestampSeconds;
+String Time;
 
 void setup() 
 {  
@@ -59,23 +68,36 @@ void setup()
   Serial.println( ip );
 
   /********************************/
-  Serial.print("Ecco il timestamp di accensione Arduino");
-  client.get("/timestamp");
-  response = client.responseStatusCode();
-  Time = response.toFloat(); 
-  Serial.print(Time);
   
+  Serial.print("Ecco il timestamp di accensione Arduino");
+  Serial.print("\n");
+  client.get("/timestamp");
+  //float StatusCode = client.responseStatusCode();
+  response = client.responseBody();
+
+  IndiceSeparatore = response.indexOf(".");
+  TimestampMicros = response.substring(IndiceSeparatore+1).toInt();
+  TimestampSeconds = response.substring(0, IndiceSeparatore).toInt();
+
+  
+  Serial.print("Timestamp del server: " + response);
+  Serial.print("\n");
+  Serial.print("Microseconds: " + String(TimestampMicros));
+  Serial.print("\n");
+  Serial.print("Seconds: " + String(TimestampSeconds));
+  Serial.print("\n");
+ 
 }
 
 void loop() 
 {
+  int T = micros();
   if (!IMU.begin()) 
   {
     Serial.println("Failed to initialize IMU!");
     while (1);
   }
 
-  int t = micros();
   if(IMU.accelerationAvailable())
   {
     IMU.readAcceleration(A_x, A_y, A_z); 
@@ -84,19 +106,19 @@ void loop()
   {
     IMU.readGyroscope(g_x, g_y, g_z); 
   }
-  int deltaT = micros()-t;
 
-  Time += 0.04;  
-  
+  //Formattazione stringa timestamp
+  Time = String(TimestampSeconds) + String(TimestampMicros);
+ 
+  // Formattazione dati da inviare al server
   StringaDati  += String(A_x) += String("@") += String(A_y) += String("@") += String(A_z) += String("@") += String(g_x) += String("@") += String(g_y) += String("@") += String(g_z) += String("@") += String(Time) += String("/");
-  
   DataMatrix += StringaDati;
-  
-  
-  
-  
-  if(i==49)
+  StringaDati = ""; 
+
+  // Codice di controllo per verificare che si è arrivati a 2 secondi di presa dati  
+  if(i==49)   // Invio dati al server
   {
+    int t = millis();
     //DataMatrix = DataMatrix.substring(DataMatrix.length() - 1);
     DataMatrix.remove(DataMatrix.length() -1);
     DataMatrix += String("\"");
@@ -114,17 +136,34 @@ void loop()
     Serial.print( "Response: " );
     Serial.println( response );
     i=0;
+    int TempoInvioServer = millis() -t;
+    Serial.print("Tempo di invio al server in millisecondi" + String(TempoInvioServer));
   }
   else
   {
     i++;
   } 
 
-  StringaDati = ""; 
-  //postData = "";
-  
-  // Aspetta un tempo di 40 millisecondi, circa, per prendere un nuovo dato                            micros andrà sostituito (causa overflow) col timestamp (che servirà dunque ad impostare anche la frequenza corretta)
-  delayMicroseconds(39800-deltaT) ;
-  
-  
+  // Costruisco un delta tempo per tener conto del tempo di presa e conversione dati del sensore, dell'invio al server e dei vari passaggi intermedi
+  int DeltaT = micros()-T;
+  int DelayTime = Periodo - DeltaT;  
+
+  if(DelayTime <= 0)
+  {
+    delayMicroseconds(-DelayTime);
+  }
+  else
+  {
+    delayMicroseconds(DelayTime);
+  }
+    
+
+  // Aggiorno il Timestamp espresso in microsecondi
+  int AddTimestamp = micros() - T;
+  TimestampMicros += AddTimestamp;
+  if(TimestampMicros >= 1000000)
+  {
+    TimestampSeconds ++;
+  }
+  TimestampMicros = TimestampMicros%1000000;
 }
